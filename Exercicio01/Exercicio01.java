c70868c9ec059c1d4bf69b39860c3b5b1cce97cc
n4rin <aline.sores2712@gmail.com>
2022-03-11 08:52:50 -0300

    change classes structure

--- Exercicio01/Exercicio01.java
+++ Exercicio01/Exercicio01.java
@@ -1,5 +1,8 @@
 package Exercicio01;
 
-public static void main(String[] args) {
-    Funcionario func1 = new Funcionario();
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Funcionario func1 = new Funcionario();
+        System.out.println("O salario do funcionário é: " + func1.getSalarioMens());
+    }
 }
