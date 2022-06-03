0b20f952beed011011f402a7e1f0b79705a49f3e
vxpresto <nallinguilherme@gmail.com>
2022-06-02 22:10:56 -0300

    beta final

--- ProjetoFloricultura/src/ProjetoFloricultura.java
+++ ProjetoFloricultura/src/ProjetoFloricultura.java
@@ -10,15 +10,15 @@ public class ProjetoFloricultura {
 	private static ArrayList<Produto> cadastroProdutos = new ArrayList<Produto>();
 
 	private static Scanner scanUsuario = new Scanner(System.in);
-	
+
 	public static Cliente criarCliente() {
 		// cria cliente
 		Cliente novoCliente = new Cliente();
-		
+
 		System.out.println("Digite o nome do Cliente: ");
 		novoCliente.setNome(scanUsuario.next());
 		scanUsuario.nextLine();
-		
+
 		while (novoCliente.getIdade() < 1) {
 			try {
 				System.out.println("Digite a idade do cliente: ");
@@ -27,7 +27,7 @@ public class ProjetoFloricultura {
 				System.out.println("Idade de cliente invÃ¡lida...");
 			}
 		}
-		
+
 		System.out.print("Digite o telefone do Cliente: \n");
 		novoCliente.setTelefone(scanUsuario.nextInt());
 
@@ -53,7 +53,8 @@ public class ProjetoFloricultura {
 							+ "[10] - Buscar Cliente\n[11] - Buscar Produto por Descricao\n[12] - Bucar Produto por preco\n[13] - Buscar"
 							+ "produto por altura\n\n[14] - Buscar Produto mais caro\n[15] - Buscar produto mais barato\n[16] - Imprimir media"
 							+ " de preco dos produtos\n[17] - Exibir produtos acima da media\n[18] - Exibir Cliente mais velho"
-							+ "\n[19] - Exibir Cliente mais novo");
+							+ "\n[19] - Exibir Cliente mais novo\n[20] - Exibir quantidade de clientes maiores de 60 anos\n[21] -"
+							+ " Exibir quantidade de clientes maiores de 18 anos\n[22] - Exibir media de idade dos clientes ");
 
 			opcaoUsuario = scanUsuario.nextInt();
 
@@ -118,16 +119,59 @@ public class ProjetoFloricultura {
 			case 19:
 				exibirClienteMaisNovo();
 				break;
+			case 20:
+				System.out
+						.println("A quantidade de clientes maiores de 60 anos e: " + retornaQtdClientesMaisSessenta());
+				break;
+			case 21:
+				System.out.println("A quantidade de clientes maiores de 18 anos e: " + retornaQtdClientesMaisDezoito());
+				break;
+			case 22:
+				System.out.println("A idade media dos clientes e: " + retornaMediaIdade());
+				break;
 			default:
-				throw new IllegalArgumentException("Opcao invalida !!");
+				System.out.println("Opcao invalida...");
 			}
 		}
 	}
 
+	private static int retornaMediaIdade() {
+		int mediaIdade = 0;
+
+		for (Cliente c : cadastroClientes) {
+			mediaIdade += c.getIdade();
+		}
+		mediaIdade = mediaIdade / cadastroClientes.size();
+		return mediaIdade;
+	}
+
+	private static int retornaQtdClientesMaisDezoito() {
+		int qtdClientesMaisDezoito = 0;
+
+		for (Cliente c : cadastroClientes) {
+			if (c.getIdade() > 18) {
+				qtdClientesMaisDezoito++;
+			}
+		}
+
+		return qtdClientesMaisDezoito;
+	}
+
+	private static int retornaQtdClientesMaisSessenta() {
+		int qtdClientesMaisSessenta = 0;
+
+		for (Cliente c : cadastroClientes) {
+			if (c.getIdade() > 60) {
+				qtdClientesMaisSessenta++;
+			}
+		}
+		return qtdClientesMaisSessenta;
+	}
+
 	private static void exibirClienteMaisNovo() {
 		Cliente clienteMaisNovo = cadastroClientes.get(0);
-		for (Cliente c: cadastroClientes) {
-			if(clienteMaisNovo.getIdade() > c.getIdade()) {
+		for (Cliente c : cadastroClientes) {
+			if (clienteMaisNovo.getIdade() > c.getIdade()) {
 				clienteMaisNovo = c;
 			}
 		}
@@ -136,7 +180,7 @@ public class ProjetoFloricultura {
 
 	private static void exibirClienteMaisVelho() {
 		Cliente clienteMaisVelho = cadastroClientes.get(0);
-		for (Cliente c: cadastroClientes) {
+		for (Cliente c : cadastroClientes) {
 			if (clienteMaisVelho.getIdade() < c.getIdade()) {
 				clienteMaisVelho = c;
 			}
@@ -145,28 +189,28 @@ public class ProjetoFloricultura {
 	}
 
 	private static void exibirPrdtsAcimaMediaPreco() {
-		for (Produto p: cadastroProdutos) {
+		for (Produto p : cadastroProdutos) {
 			if (p.getPreco() > retornaMediaPrecos()) {
 				p.exibir();
 			}
 		}
-		
+
 	}
 
 	private static double retornaMediaPrecos() {
 		double mediaPrecos = 0;
-		
-		for (Produto p: cadastroProdutos) {
+
+		for (Produto p : cadastroProdutos) {
 			mediaPrecos += p.getPreco();
 		}
-		
+
 		mediaPrecos = mediaPrecos / cadastroClientes.size();
 		return mediaPrecos;
 	}
 
 	private static void imprimeProdutoBarato() {
 		Produto prodMaisBarato = cadastroProdutos.get(0);
-		for (Produto p: cadastroProdutos) {
+		for (Produto p : cadastroProdutos) {
 			if (prodMaisBarato.getPreco() > p.getPreco()) {
 				prodMaisBarato = p;
 			}
@@ -176,7 +220,7 @@ public class ProjetoFloricultura {
 
 	private static void imprimeProdutoCaro() {
 		Produto prodMaisCaro = new Produto();
-		for (Produto p: cadastroProdutos) {
+		for (Produto p : cadastroProdutos) {
 			if (prodMaisCaro.getPreco() < p.getPreco()) {
 				prodMaisCaro = p;
 			}
@@ -187,9 +231,9 @@ public class ProjetoFloricultura {
 	private static void buscarProdutoAltura() {
 		System.out.println("Para realizar a busca digite a altura da planta: ");
 		double produtoAltura = scanUsuario.nextDouble();
-		
-		for (Produto p: cadastroProdutos) {
-			if(p.getAltura() == produtoAltura) {
+
+		for (Produto p : cadastroProdutos) {
+			if (p.getAltura() == produtoAltura) {
 				p.exibir();
 			}
 		}
@@ -198,8 +242,8 @@ public class ProjetoFloricultura {
 	private static void buscarProdutoPreco() {
 		System.out.println("Para realizar a busca digite o preco da planta: ");
 		double produtoPreco = scanUsuario.nextDouble();
-		
-		for (Produto p: cadastroProdutos) {
+
+		for (Produto p : cadastroProdutos) {
 			if (p.getPreco() == produtoPreco) {
 				p.exibir();
 			}
@@ -210,8 +254,8 @@ public class ProjetoFloricultura {
 		System.out.println("Para realizar a busca, digite, no minimo, parte da descricao da planta: ");
 		String produtoIniciais = scanUsuario.next();
 		scanUsuario.nextLine();
-		
-		for (Produto p: cadastroProdutos) {
+
+		for (Produto p : cadastroProdutos) {
 			if (p.getDescricao().startsWith(produtoIniciais)) {
 				p.exibir();
 			}
@@ -221,9 +265,8 @@ public class ProjetoFloricultura {
 	private static void buscarCliente() {
 		System.out.println("Para realizar a busca, digite, no minimo parte do nome do cliente: ");
 		String clienteIniciais = scanUsuario.next();
-		
-		
-		for (Cliente c: cadastroClientes) {
+
+		for (Cliente c : cadastroClientes) {
 			if (c.getNome().startsWith(clienteIniciais)) {
 				c.exibir();
 			}
@@ -232,21 +275,21 @@ public class ProjetoFloricultura {
 
 	private static void apagarProduto() {
 		System.out.println("Digite o índice do produto a ser apagado: ");
-		
+
 		int indexProdutoApagar = scanUsuario.nextInt();
-		
+
 		while (indexProdutoApagar > cadastroProdutos.size() || indexProdutoApagar < 0) {
 			System.out.println("Numero de funcionario invalido...");
-			
+
 			System.out.println("\nDigite novamente o numero do funcionario: ");
 			indexProdutoApagar = scanUsuario.nextInt();
 		}
 		cadastroFuncionarios.remove(indexProdutoApagar);
-		
+
 	}
 
 	private static void exibirListaProdutos() {
-		for (Produto p: cadastroProdutos) {
+		for (Produto p : cadastroProdutos) {
 			p.exibir();
 			System.out.println("Numero do produto: " + cadastroProdutos.indexOf(p) + "\n");
 		}
@@ -254,12 +297,12 @@ public class ProjetoFloricultura {
 
 	private static Produto criarProduto() {
 		Produto novoProduto = new Produto();
-		
+
 		System.out.println("Digite a descricao da planta: ");
 		novoProduto.setDescricao(scanUsuario.next());
-		
+
 		scanUsuario.nextLine();
-		
+
 		while (novoProduto.getPreco() < 1) {
 			try {
 				System.out.println("Digite o preco da planta: ");
@@ -270,16 +313,16 @@ public class ProjetoFloricultura {
 				System.out.println("Tipo de preço inválido... ");
 			}
 		}
-		
+
 		while (novoProduto.getPeso() < 1) {
 			try {
 				System.out.println("Digite o peso da planta: ");
-				novoProduto.setPeso(scanUsuario.nextDouble());			
+				novoProduto.setPeso(scanUsuario.nextDouble());
 			} catch (IllegalArgumentException e) {
 				System.out.println("Preco invalido... ");
 			}
 		}
-		
+
 		while (novoProduto.getAltura() < 1) {
 			try {
 				System.out.println("Digite a altura da planta: ");
@@ -288,21 +331,21 @@ public class ProjetoFloricultura {
 				System.out.println("Altura invalida... ");
 			}
 		}
-		
+
 		System.out.println("Digite a especie da planta... ");
 		novoProduto.setEspecie(scanUsuario.next());
-		
+
 		scanUsuario.nextLine();
-		
+
 		return novoProduto;
 	}
 
 	private static void exibirListaFuncionarios() {
-		for (Funcionario f: cadastroFuncionarios) {
+		for (Funcionario f : cadastroFuncionarios) {
 			f.exibir();
-			System.out.print("NÃºmero do funcionario: " + cadastroFuncionarios.indexOf(f) + "\n");			
+			System.out.print("NÃºmero do funcionario: " + cadastroFuncionarios.indexOf(f) + "\n");
 		}
-		
+
 	}
 
 	private static Funcionario criarFuncionario() {
@@ -324,7 +367,7 @@ public class ProjetoFloricultura {
 		System.out.println("Digite o cargo do funcionÃ¡rio: ");
 		novoFuncionario.setCargo(scanUsuario.next());
 		scanUsuario.nextLine();
-		
+
 		while (novoFuncionario.getSalario() < 0) {
 			try {
 				System.out.println("Digite o salario do funcionario: ");
@@ -335,7 +378,7 @@ public class ProjetoFloricultura {
 				System.out.println("Tipo de salario invalido... ");
 			}
 		}
-		
+
 		System.out.println("Digite a quantidade de horas semanais do funcionario: ");
 		novoFuncionario.setHorasSemanais(scanUsuario.nextInt());
 
@@ -344,25 +387,25 @@ public class ProjetoFloricultura {
 
 	private static void apagarFuncionario() {
 		System.out.println("Digite o índice do funcionário a ser apagado: ");
-		
+
 		int indexFuncionarioApagar = scanUsuario.nextInt();
-		
+
 		while (indexFuncionarioApagar > cadastroFuncionarios.size() || indexFuncionarioApagar < 0) {
 			System.out.println("Numero de funcionario invalido...");
-			
+
 			System.out.println("\nDigite novamente o numero do funcionario: ");
 			indexFuncionarioApagar = scanUsuario.nextInt();
 		}
 		cadastroFuncionarios.remove(indexFuncionarioApagar);
 	}
-	
+
 	private static void apagarCliente() {
 		System.out.println("Digite o Ã­ndice do cliente a ser apagado: ");
 
 		int indexClienteApagar = scanUsuario.nextInt();
 		while (indexClienteApagar > cadastroClientes.size() || indexClienteApagar < 0) {
 			System.out.println("Numero de cliente invalido...");
-			
+
 			System.out.println("\nDigite novamente o numero do cliente: ");
 			indexClienteApagar = scanUsuario.nextInt();
 		}
