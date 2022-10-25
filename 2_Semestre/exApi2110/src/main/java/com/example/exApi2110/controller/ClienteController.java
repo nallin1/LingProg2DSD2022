06cff285e9cad7a20d9e47f2400a5a8eba8ab2d0
vxpresto <nallinguilherme@gmail.com>
2022-10-25 15:02:26 -0300

    floriculturaapi

--- 2_Semestre/exApi2110/src/main/java/com/example/exApi2110/controller/ClienteController.java
+++ 2_Semestre/exApi2110/src/main/java/com/example/exApi2110/controller/ClienteController.java
@@ -2,12 +2,16 @@ package com.example.exApi2110.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.exApi2110.model.Cliente;
 import com.example.exApi2110.repository.ClienteRepository;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/apiClienteFloricultura")
@@ -18,5 +22,20 @@ public class ClienteController {
     @GetMapping(value = "/buscarClientes")
     public List<Cliente> buscarClientes() {
         return clienteRep.findAll();
+    }
+
+    @PostMapping("/cadastrarCliente")
+    public void cadastrarCliente(@RequestBody Cliente cliente) {
+        clienteRep.save(cliente);
+    }
+
+    @GetMapping(value = "/buscarClientesId/{id}")
+    public Optional<Cliente> buscarClienteId(@PathVariable(value = "id") int id) {
+        return clienteRep.findById(id);
+    }
+
+    @PostMapping("/inserir")
+    public void inserirCliente(@RequestBody Cliente cliente) {
+        clienteRep.save(cliente);
     }
 }
